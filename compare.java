323248b6b454065c1d877add436c52ba57cc2b8f
Adhithyas27 <Adhithyas27@gmail.com>
2025-09-08 15:12:56 +0530

    added compare.java and typecasting.java

---
+++ compare.java
@@ -0,0 +1,9 @@
+public class compare {
+    public static void main(String[] args) {
+        int a,b;
+        a=120;
+        b=123;
+        boolean c =a<b;
+        System.out.println(c);
+    }
+}
---
+++ typecasting.java
@@ -0,0 +1,13 @@
+public class typecasting {
+  public static void main(String[] args) {
+    int adf = 34345;
+    long aff =(long)adf;
+    System.out.println(aff);
+    String an= "ROSSawtawr";
+    for(int i=0;i<an.length();i++){
+        System.out.println((int)an.charAt(i));
+    }
+
+
+  }  
+}
