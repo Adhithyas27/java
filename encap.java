5f475954b388e9427a3f73ad7efaa5c6909e2021
Adhithyas27 <Adhithyas27@gmail.com>
2025-09-12 19:31:38 +0530

    files are added

---
+++ encap.java
@@ -0,0 +1,29 @@
+class Human{
+    private int number;
+    private String name;
+
+    public void setNumber(int a){
+        number = a;
+    }
+    public void setName(String b){
+        name = b;
+    }
+    public int getNumber(){
+        return number;
+    }
+     public String getName(){
+        return name;
+    }
+}
+
+public class encap { 
+    public static void main(String[] args){
+        Human h1 = new Human();
+        h1.setName("Adhithya");
+        h1.setNumber(9000);
+        System.out.println(h1.getName() + " get");
+        System.out.println(h1.getNumber() + " get");
+        
+    }
+    
+}
