07fbd2a0a191f2e658ee40f021dacd74d3ef386e
Adhithyas27 <Adhithyas27@gmail.com>
2025-10-28 16:21:21 +0530

    files are added

---
+++ Inter.java
@@ -0,0 +1,24 @@
+interface A{
+    int age = 90;
+    String area = "Mumbai";
+    void show();
+    void config();
+
+}
+class B implements A{
+    public void show(){
+        System.out.println("in show...");
+    }
+    public void config(){
+        System.out.println("in comfig...");
+    }
+}
+
+public class Inter {
+    public static void main(String[] args){
+        A obj = new B();
+        obj.show();
+        obj.config();
+    }
+    
+}
