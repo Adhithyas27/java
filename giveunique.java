65b96fbeb044a1c0bc6f90ce8d719cdd6459849a
Adhithyas27 <Adhithyas27>
2025-11-11 14:59:44 +0530

    files are added

---
+++ giveunique.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class giveunique {
+public static void main(String[] args){
+    int[][] a={{25,8,5,28},{8,25,28,8}};
+    TreeMap<Integer,Integer> adhi = new TreeMap<>();
+    for(int i=0;i<a.length;i++){
+        for(int j=0;j<a[i].length;j++){
+            adhi.put(a[i][j],adhi.getOrDefault(a[i][j],0)+1);
+        }
+    }
+    System.out.println(adhi);
+    for(Integer aa : adhi.keySet()){
+        if(adhi.get(aa)==1){
+            System.out.println(aa+":"+adhi.get(aa));
+        }
+
+    }
+    
+}
+    
+}
